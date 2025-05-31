80b910803f3bdca1e7d424c3739722ebe345dc75
Aya <ayahathout1@gmail.com>
2025-05-31 16:27:29 +0300

    remove some from repo of user

--- misk/src/main/java/iti/jets/misk/repositories/UserRepository.java
+++ misk/src/main/java/iti/jets/misk/repositories/UserRepository.java
@@ -3,13 +3,8 @@ package iti.jets.misk.repositories;
 import iti.jets.misk.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    List<User> findAll(); // It should not return DTO
-
-    Optional<User> findById(int id);
-
     Optional<User> findByEmail(String email);
 }
